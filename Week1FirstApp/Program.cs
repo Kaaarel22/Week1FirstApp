6317460de8190be33691795668c84235ec5d1a10
Kaarel Kolter <kaarelkolter@tktk.ee>
2024-10-05 17:07:46 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,7 @@
+﻿//This is a comment
+
+string myName = "Kaarel";
+string myLastname = "Kolter";
+//Console.WriteLine("Hello" + " " + myName);
+
+Console.WriteLine($"Hello, {myName} {myLastname}!");
